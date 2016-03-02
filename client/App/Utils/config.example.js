95f3b853fd389ee15761100f498237efcd4e6e76
Colin Whitmarsh <colin.whitmarsh@gmail.com>
2016-03-01 18:05:57 -0800

    (setup) added config.example.js

---
+++ client/App/Utils/config.example.js
@@ -0,0 +1,11 @@
+var config = {};
+
+//Set url for API
+  // First, rename this file to 'config.js'. It will be ignored by git
+  // Then, set the url below
+  // To build on simulator, set to 'localhost'
+  // To build on an external iPhone, set to your IP address. System Preferences > Nextwork > Advanced Settings > TCP/IP > XX.X.XX.XXX
+
+config.url = 'localhost'
+
+module.exports = config;
--- client/App/Utils/config.js
+++ client/App/Utils/config.js
@@ -4,6 +4,6 @@ var config = {};
   // To build on simulator, set url to 'localhost'
   // To build on an external iPhone, set url to your IP address. System Preferences > Nextwork > Advanced Settings > TCP/IP > XX.X.XX.XXX
 
-config.url = 'localhost'
+config.url = '10.6.30.148'
 
 module.exports = config;
