d103db984790a3e330203c0ab745e2c3cd1a0f5a
Colin Whitmarsh <colin.whitmarsh@gmail.com>
2016-03-01 17:55:00 -0800

    (setup) removed config from git tracking

--- client/App/Utils/config.js
+++
@@ -1,9 +0,0 @@
-var config = {};
-
-//Set url for API
-  // To build on simulator, set url to 'localhost'
-  // To build on an external iPhone, set url to your IP address. System Preferences > Nextwork > Advanced Settings > TCP/IP > XX.X.XX.XXX
-
-config.url = 'localhost'
-
-module.exports = config;
