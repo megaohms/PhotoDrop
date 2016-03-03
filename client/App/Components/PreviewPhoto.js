e096882a10fb5a7fecbf3bf41f3277106fa5ac3b
Vince Cipriani <vince.cipriani@gmail.com>
2016-03-03 10:56:22 -0800

    add functions to send image for all users and only friends

--- client/App/Components/PreviewPhoto.js
+++ client/App/Components/PreviewPhoto.js
@@ -28,15 +28,24 @@ class PreviewPhoto extends React.Component {
     };
   }
 
-  _sendImage() {
-    api.uploadPhoto(this.props.route.image64, this.props.route.latitude, this.props.route.longitude, this.props.route.userId, (res) => {
-      this.setState({modalVisible: true});
-      setTimeout(()=> {
-        this._closeModal();
-      }, 1300);
-    });
+  _sendImage(visibility) {
+    api.uploadPhoto(this.props.route.image64, this.props.route.latitude, this.props.route.longitude
+        , this.props.route.userId, visibility, 
+        (res) => {
+          this.setState({modalVisible: true});
+          setTimeout(()=> {
+            this._closeModal();
+          }, 1300);
+        });
   }
 
+  _sendImageForAll() {
+    this._sendImage(2);
+  }
+  
+  _sendImageForFriends() {
+    this._sendImage(1);
+  }
   _closeModal() { 
     this.setState({modalVisible: false});
     this.props.navigator.pop();
@@ -70,10 +79,10 @@ class PreviewPhoto extends React.Component {
             <TouchableOpacity onPress={_.once(this._cancelImage.bind(this))} style={styles.noButton}>
               <IconIon name="ios-close-empty" size={60} color="#FC9396" style={styles.noIcon} />
             </TouchableOpacity>
-            <TouchableOpacity onPress={_.once(this._sendImage.bind(this))} >
+            <TouchableOpacity onPress={_.once(this._sendImageForAll.bind(this))} >
               <IconIon name="earth" size={60} color="#036C69" style={styles.earthButton} />
             </TouchableOpacity>
-            <TouchableOpacity onPress={_.once(this._sendImage.bind(this))} style={styles.friendsButton}>
+            <TouchableOpacity onPress={_.once(this._sendImageForFriends.bind(this))} style={styles.friendsButton}>
               <IconIon name="ios-people" size={60} color="#036C69" style={styles.yesIcon} />
             </TouchableOpacity>
           </View>
