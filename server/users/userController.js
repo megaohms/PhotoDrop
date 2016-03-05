ad9563f4c1f17b748c1b90725ec9ac111d101b8f
Megan Ring <meganbethring@gmail.com>
2016-03-05 13:49:45 -0800

    define new getUserStreamsObjects function for retrieving new userModel property

--- server/users/userController.js
+++ server/users/userController.js
@@ -235,6 +235,27 @@ module.exports = {
 
   fetchStreamsObject: function(req, res, next){
     //fetch streamsObject with photo_id and .populate method, see fetch friends below
+    var streamsPhotosArr = [];
+    
+    User.findOne({ _id: mongoose.mongo.ObjectID(req.query.userId) })
+      .populate('streamsObjects', 'url loc visibility')
+      .exec(function (err, user) {
+        if (err) {
+          next(err);
+        } else {
+          for (var i = 0; i < user.streamsObjects.length; i++) {
+            streamsPhotosArr.push({
+              url: user.streamsObjects[i].url,
+              latLng: {
+                latitude: user.streamsObjects[i].loc.coordinates[0],
+                longitude: user.streamsObjects[i].loc.coordinates[1]
+              },
+              visibility: user.streamsObjects[i].visibility
+            });
+          }
+          res.json(streamsPhotosArr);
+        }
+      });
   },
   
   fetchUsersBySearchInput: function(req, res, next) {
